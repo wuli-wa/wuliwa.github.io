fdd25f1d01578f164c4002d16da8fd52e0c3bdb6
wuli-wa <1161353741@qq.com>
2026-07-31 01:20:02 +0800

    Site updated: 2026-07-31 01:19:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/08/Mirage-Tank/","2026/07/11/STEGA-LSB/","2026/07/10/STEGA-直接附加/","2026/04/10/hello-world/","2026/07/07/芝士甚么-WIFI密码/"];function toRandomPost(){
+var posts=["2026/07/08/Mirage-Tank/","2026/07/10/STEGA-直接附加/","2026/07/11/STEGA-LSB/","2026/07/07/芝士甚么-WIFI密码/","2026/04/10/hello-world/","2026/07/23/zip伪加密/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
